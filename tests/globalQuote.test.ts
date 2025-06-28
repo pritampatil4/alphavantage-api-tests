376a4d8fd0c325bbcf29a652af9682201a2d811a
Pritam Patil <ppritam821@gmail.com>
2025-06-28 13:34:12 +0200

    update test toBe assertion

--- tests/globalQuote.test.ts
+++ tests/globalQuote.test.ts
@@ -1,3 +1,4 @@
+
 import { AlphaVantageClient } from "../src/api/alphaAvantageClient";
 import {
     GlobalQuoteAPIResponse,
@@ -13,30 +14,36 @@ beforeAll(() => {
 describe("AlphaVantage Global Quote API Tests", () => {
     test("Should return a global quote for a valid symbol (MSFT) or indicate rate limit", async () => {
         const symbol = "MSFT";
+        const responseKeys = [
+            '01. symbol',
+            '02. open',
+            '03. high',
+            '04. low',
+            '05. price',
+            '06. volume',
+            '07. latest trading day',
+            '08. previous close',
+            '09. change',
+            '10. change percent'
+        ]
         try {
             const response: GlobalQuoteAPIResponse = await apiClient.getGlobalQuote(symbol);
             expect(response).toBeDefined();
+            expect(typeof response).toBe("object");
             expect(response).toHaveProperty("Global Quote");
             const globalQuote = (response as { "Global Quote": GlobalQuoteData })["Global Quote"];
-            expect(globalQuote).toHaveProperty("01. symbol");
-            expect(globalQuote["01. symbol"]).toBe(symbol);
-            expect(globalQuote).toHaveProperty("02. open");
-            expect(globalQuote).toHaveProperty("03. high");
-            expect(globalQuote).toHaveProperty("04. low");
-            expect(globalQuote).toHaveProperty("05. price");
-            expect(globalQuote).toHaveProperty("06. volume");
-            expect(globalQuote).toHaveProperty("07. latest trading day");
+            expect(typeof globalQuote).toBe("object");
+            expect(globalQuote).not.toBeNull();
+            expect(globalQuote["01. symbol"]).toBe("MSFT");
+            expect(Object.keys(globalQuote)).toEqual(responseKeys);
             expect(globalQuote["07. latest trading day"]).toMatch(/^\d{4}-\d{2}-\d{2}$/);
-            expect(globalQuote).toHaveProperty("08. previous close");
-            expect(globalQuote).toHaveProperty("09. change");
-            expect(globalQuote).toHaveProperty("10. change percent");
         } catch (error) {
             if (error instanceof AlphaVantageAPIRateLimitError) {
                 console.warn(
                     `API Rate Limit Detected for ${symbol} (Test Passed by Design): ${error.message}`
                 );
             } else {
-                fail(`Test failed with unexpected error: ${error}`);
+                throw error;
             }
         }
     });
